a77f0f585c86908b7c72650621a99e4fa3b2a3b2
Aream <aream@geomais.com.br>
2022-05-29 12:31:13 -0300

    feat: add file to put dev infos

---
+++ src/data/DevData.ts
@@ -0,0 +1,10 @@
+import { IDevData } from "./IDevData";
+
+export const devData: IDevData.Full = {
+    person: {
+        name: "Aream",
+        surname: "Luersen",
+        title: "Full Stack Developer",
+        githubAccount: "areamluersen"
+    }
+}
