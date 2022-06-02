5388d23d001d3a3edc24f75dd7b8ff8431d50e94
Aream Luersen <areamluersen@gmail.com>
2022-06-01 21:35:34 -0300

    feat: reading data from json

--- src/components/NewLayout/index.tsx
+++ src/components/NewLayout/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { GithubLogo, InstagramLogo, LinkedinLogo, RocketLaunch } from 'phosphor-react';
+import { devData } from '../../data/DevData';
 
 const NewLayout: React.FC = () => {
+  const { person } = devData;
   return (
     <div className="bg-dark-950 min-h-screen w-full  h-full text-primary font-roboto">
       <div className="px-8 h-20 w-full flex align-middle items-center justify-between">
@@ -26,25 +28,44 @@ const NewLayout: React.FC = () => {
       <div className="flex w-full mt-10 px-8 align-middle items-center">
         <div className="">
           <div className=" grid grid-cols-1">
-            <span className="text-5xl font-medium leading-tight pb-6">Dhonny Jeep</span>
-            <span className="text-3xl font-light">Full-Stack Developer</span>
+            <span className="text-5xl font-medium leading-tight pb-6">
+              {person.name} {person.surname}
+            </span>
+            <span className="text-3xl font-light">{person.title}</span>
             <span className="w-1/4 pt-2 border-b hover:w-2/3 transition-all duration-500 ease-in-out" />
           </div>
           <div className="py-28">
-            <button className="text-2xl font-normal transition-all duration-700 ease-in-out border hover:bg-primary hover:text-black p-2">
+            <button
+              onClick={() =>
+                open(`https://github.com/${person.githubAccount}?tab=repositories`)
+              }
+              className="text-2xl font-normal transition-all duration-700 ease-in-out border hover:bg-primary hover:text-black p-2"
+            >
               My Projects
             </button>
           </div>
           <div className="text-2xl font-light flex transition-all duration-700 ease-in-out">
-            <button className="flex items-center transition-all duration-500 ease-in-out hover:text-linkedin">
+            <a
+              href={person.linkedInAccount}
+              target="_blank"
+              className="flex items-center transition-all duration-500 ease-in-out hover:text-linkedin"
+            >
               <LinkedinLogo className="w-8" /> linkedin
-            </button>
-            <button className="flex items-center transition-all duration-500 ease-in-out hover:text-violet-500">
+            </a>
+            <a
+              href={`https://github.com/${person.githubAccount}`}
+              target="_blank"
+              className="flex items-center transition-all duration-500 ease-in-out hover:text-violet-500"
+            >
               <GithubLogo className="w-8" /> github
-            </button>
-            <button className="flex items-center transition-all duration-500 ease-in-out hover:text-instagram">
+            </a>
+            <a
+              href={person.instagramAccount}
+              target="_blank"
+              className="flex items-center transition-all duration-500 ease-in-out hover:text-instagram"
+            >
               <InstagramLogo className="w-8" /> instagram
-            </button>
+            </a>
           </div>
         </div>
       </div>
