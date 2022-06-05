7e20a5de16feb6174c12edeac2261265a6b917d3
Aream Luersen <areamluersen@gmail.com>
2022-06-05 19:52:11 -0300

    feat: following layout figma mobile

--- src/components/NewLayout/index.tsx
+++ src/components/NewLayout/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { GithubLogo, InstagramLogo, LinkedinLogo, RocketLaunch } from 'phosphor-react';
 import { devData } from '../../data/DevData';
+import { UserAvatar } from '../UserAvatar';
 
 const NewLayout: React.FC = () => {
   const { person } = devData;
   return (
     <div className="bg-dark-950 min-h-screen w-full  h-full text-primary font-roboto">
-      <div className="px-8 h-20 w-full flex align-middle items-center justify-between">
-        <div className="flex">
+      <div className="px-8 h-20 w-full flex align-middle items-center justify-between header-space-commented-in-simple-design">
+        {/* <div className="flex">
           <RocketLaunch size={32} />
         </div>
         <div className="">
@@ -19,52 +20,71 @@ const NewLayout: React.FC = () => {
           <button className="text-sm sm:text-xl lg:text-2xl font-light pl-2">
             Contact
           </button>
-        </div>
+        </div> */}
       </div>
-      <div className="flex w-full mt-10 px-8 align-middle items-center">
+      <div className="flex w-full mt-10 px-6 sm:px-16 md:px-28 align-middle items-center">
         <div className="">
-          <div className=" grid grid-cols-1">
-            <span className="text-4xl sm:text-5xl font-medium leading-tight pb-6">
-              {person.name} {person.surname}
-            </span>
-            <span className="text-2xl sm:text-3xl font-light hover:hr">
-              {person.title}
-            </span>
-            <hr className="w-1/4 h-2 hover:w-2/3 transition-all duration-500 ease-in-out h" />
-          </div>
-          <div className="py-28">
-            <button
-              onClick={() =>
-                open(`https://github.com/${person.githubAccount}?tab=repositories`)
-              }
-              className="text-xl sm:text-2xl font-normal transition-all duration-700 ease-in-out border hover:bg-primary hover:text-black p-2"
-            >
-              My Projects
-            </button>
+          <div className="sm:flex sm:flex-row-reverse sm:justify-between">
+            <div className="flex justify-center mb-8 sm:mb-0 col-">
+              <UserAvatar className="w-36 h-36 sm:w-64 sm:h-64 lg:w-72 lg:h-72" />
+            </div>
+            <div className="sm:mr-12 min-w-[250px]">
+              <div className="">
+                <div className="mb-6">
+                  <span className="text-3xl sm:text-5xl font-medium leading-tight">
+                    {person.name} {person.surname}
+                  </span>
+                </div>
+                <div>
+                  <span className="text-2xl sm:text-4xl font-light">{person.title}</span>
+                  <div className="pt-2 pb-5 sm:pb-8">
+                    <hr className="w-1/6 hover:w-2/6 transition-all duration-500 ease-in-out border-dark-700 " />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <span className="text-base sm:text-2xl font-thin">
+                  {person.shortDescription}
+                </span>
+              </div>
+              <div className="flex justify-center sm:justify-start py-9 sm:py-28">
+                <button
+                  onClick={() =>
+                    open(`https://github.com/${person.githubAccount}?tab=repositories`)
+                  }
+                  className="text-xl sm:text-2xl font-normal transition-all duration-700 ease-in-out border hover:bg-primary hover:text-black p-2"
+                >
+                  My Projects
+                </button>
+              </div>
+            </div>
           </div>
           <div
-            className={`grid-rows-${4} sm:flex text-2xl font-light transition-all duration-700 ease-in-out overflow-hidden`}
+            className={`flex justify-center text-2xl font-light transition-all duration-700 ease-in-out overflow-hidden`}
           >
             <a
               href={person.linkedInAccount}
               target="_blank"
-              className="flex items-center transition-all duration-500 ease-in-out hover:text-linkedin hover:bg-primary rounded hover:px-2"
+              className="flex items-center transition-all duration-500 ease-in-out text-linkedin sm:text-primary hover:text-linkedin hover:bg-primary rounded hover:px-2 px-2 sm:px-1"
             >
-              <LinkedinLogo className="w-8" /> linkedin
+              <LinkedinLogo className="w-8" />
+              <span className="w-0 sm:w-full invisible sm:visible">linkedin</span>
             </a>
             <a
               href={`https://github.com/${person.githubAccount}`}
               target="_blank"
-              className="flex items-center transition-all duration-500 ease-in-out hover:text-violet-500 hover:bg-primary rounded hover:px-2"
+              className="flex items-center transition-all duration-500 ease-in-out sm:text-primary hover:text-violet-500 hover:bg-primary rounded hover:px-2 px-2 sm:px-1"
             >
-              <GithubLogo className="w-8" /> github
+              <GithubLogo className="w-8" />{' '}
+              <span className="w-0 sm:w-full invisible sm:visible">github</span>
             </a>
             <a
               href={person.instagramAccount}
               target="_blank"
-              className="flex items-center transition-all duration-500 ease-in-out hover:text-instagram hover:bg-primary rounded hover:px-2"
+              className="flex items-center transition-all duration-500 ease-in-out text-instagram sm:text-primary hover:text-instagram hover:bg-primary rounded hover:px-2 px-2 sm:px-1"
             >
-              <InstagramLogo className="w-8" /> instagram
+              <InstagramLogo className="w-8 " />
+              <span className="w-0 sm:w-full invisible sm:visible">instagram</span>
             </a>
           </div>
         </div>
